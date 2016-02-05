89811122d373e81413087e53118b7e0a6c25846d
TheoQuimby <TheoQuimby@158.104.186.152>
2016-02-05 11:08:33 -0800

    First commit

---
+++ src/Board.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TheoQuimby
+ */
+public class Board {
+    
+    //initilize 2D array for board
+    int[][] sqs;
+    
+    //static varbiable to set board size
+    final static int SIZE=3;
+    
+    //initial constructor
+    public Board(){
+        sqs = new int[SIZE][SIZE];
+    }
+    
+}
---
+++ src/hexapawn/Hexapawn.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexapawn;
+
+/**
+ *
+ * @author TheoQuimby
+ */
+public class Hexapawn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+    
+}
